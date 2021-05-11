98d3e549cdf2f9458f5c9ab2ac8a4cdec4843bcd
cristobal1111 <cgonv22@gmail.com>
2021-05-11 11:45:05 -0400

    API finalizada

--- map.js
+++ map.js
@@ -1,9 +1,11 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.0336892, lng: -71.5331841 },
-    zoom: 15,
+function iniciarMap(){
+  var coord = {lat:-33.0336892 ,lng: -71.5331841};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
   });
 }
-
