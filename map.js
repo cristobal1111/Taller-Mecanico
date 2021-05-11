3ca80e40569b199cbb1edd19554c25530adb1324
cristobal1111 <cgonv22@gmail.com>
2021-05-11 11:59:30 -0400

    APi finalizada corregida

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function initMap(){
   var coord = {lat:-33.0336892 ,lng: -71.5331841};
   var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 10,
+    zoom: 17,
     center: coord
   });
   var marker = new google.maps.Marker({
